9874887f7a9a78f6a575b7c3e8c363f7c0ad882d
carden <carden.ruby@gmail.com>
2020-04-12 23:47:41 +0300

    en commit

--- automatic/01_even_odd.rb
+++ automatic/01_even_odd.rb
@@ -1,6 +1,7 @@
 # Дан целочисленный массив. Необходимо вывести вначале его элементы с чётными
 # индексами, а затем — с нечётными.
-#
+# The program first displays its array elements with even indices,
+# and then with odd ones.
 array = [2, -2, 3, 0, 7, 2, 23, -1, 1, 4]
 even, odd = array.partition(& :even?)
 puts even.inspect
