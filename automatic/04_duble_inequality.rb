01393e7caf81f57cb8fefb6a2f28714a74c966b3
carden <carden.ruby@gmail.com>
2020-04-13 21:31:16 +0300

    first commit

---
+++ automatic/04_duble_inequality.rb
@@ -0,0 +1,9 @@
+# Дан целочисленный массив. Вывести номер последнего из тех его элементов,
+# которые удовлетворяют двойному неравенству: A[0] < A[i] < A[n−1].
+# Если таких элементов нет, то вывести [ ].
+# The program displays the number of the last of those elements of the array
+# that satisfy double inequality.
+arr = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+prod = []
+arr.each_index { |i| prod << i if arr[0] < arr[i] && arr[i] < arr[- 1] } || []
+puts prod.last
