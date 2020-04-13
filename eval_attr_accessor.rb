8550c3ee33d38bf3e8c431740bee7f4b141d4839
carden <carden.ruby@gmail.com>
2020-04-13 23:06:21 +0300

    comment

--- eval_attr_accessor.rb
+++ eval_attr_accessor.rb
@@ -1,6 +1,9 @@
 class X
+  # Неправильный метод создаёт геттер и сеттер для переданного аргумента
   def eval_attr_accessor(*methods)
+    # Аргумент передаётся в блок
     methods.each do |method|
+      # eval выполняет строку и создаёт 2 метода геттер и сеттер
       eval "def #{method}
               @#{method}
             end
