dab9b022132f219f4e2ea1422b35d06b5b658d88
carden <carden.ruby@gmail.com>
2020-04-15 22:25:37 +0300

    first commit

---
+++ automatic/13_rotate_left_1.rb
@@ -0,0 +1,5 @@
+# Дан целочисленный массив. Осуществить циклический сдвиг элементов массива
+# влево на одну позицию.
+# The program performs a cyclic shift of array elements one position to the left
+array = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+puts array.rotate!(1).inspect
