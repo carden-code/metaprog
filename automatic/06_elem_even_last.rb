6d170aad0f4ba27690749dde7c35fd5526edca8c
carden <carden.ruby@gmail.com>
2020-04-15 22:01:18 +0300

    each -> map

--- automatic/06_elem_even_last.rb
+++ automatic/06_elem_even_last.rb
@@ -3,7 +3,7 @@
 # The program converts the array by adding the last element to even numbers.
 # The first and last elements of the array do not change.
 array = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
-array = array.each_with_index do |element, index|
+array.map.with_index do |element, index|
   next unless index.positive? && index < array.size - 1
 
   array[index] += array.last if element.even?
