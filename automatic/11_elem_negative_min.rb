692143f785f5ee6d1e11eebdb2e9d484318789be
carden <carden.ruby@gmail.com>
2020-04-15 22:19:29 +0300

    first commit

---
+++ automatic/11_elem_negative_min.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Заменить все отрицательные элементы на значение
+# минимального.
+# The program changes all negative elements to a value minimum.
+arr = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+arr.map.with_index { |elem, index| arr[index] = arr.min if elem.negative? }
+puts "Product: #{arr}"
