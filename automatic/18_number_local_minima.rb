f9d20462d60b972d51594d22f9919c25bf735481
carden <carden.ruby@gmail.com>
2020-04-16 23:15:22 +0300

    local minima

---
+++ automatic/18_number_local_minima.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Найти количество его локальных минимумов.
+# The program finds the number of local minima of the array.
+arr = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+min = []
+arr.each_with_index { |elem, index| min << elem if arr[index] < arr[index - 1] }
+puts "The number of local minima of the array: #{min.size}"
