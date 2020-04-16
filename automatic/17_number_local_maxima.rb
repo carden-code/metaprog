e44d59a4d78a061090f5fd5c9b562682e3ee37c3
carden <carden.ruby@gmail.com>
2020-04-16 23:11:44 +0300

    maxima local

---
+++ automatic/17_number_local_maxima.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Найти количество его локальных максимумов.
+# The program finds the number of local maxima of the array.
+arr = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+max = []
+arr.each_with_index { |elem, index| max << elem if arr[index] > arr[index - 1] }
+puts "The number of local maxima of the array: #{max.size}"
