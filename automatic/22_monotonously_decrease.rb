bea4bcfce833c5d6df991550a8c041d20ff6ff31
carden <carden.ruby@gmail.com>
2020-04-17 20:06:33 +0300

    first commit

---
+++ automatic/22_monotonously_decrease.rb
@@ -0,0 +1,8 @@
+# Дан целочисленный массив. Определить количество участков,
+# на которых его элементы монотонно убывают.
+# The program determines the number of sections
+# on which array elements monotonically decrease.
+arr = [2, -2, 3, 0, 7, 2, 23, -1, 3, 1, 4]
+prod = []
+arr.each_with_index { |elem, index| prod << elem if arr[index] <= arr[index - 1] }
+puts "Number of sections: #{prod.size}"
