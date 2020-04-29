9391187ad885565a08686ebdc5e2ab4c291f15c6
carden <carden.ruby@gmail.com>
2020-04-29 23:06:13 +0300

    modification

--- automatic/23_real_number_r.rb
+++ automatic/23_real_number_r.rb
@@ -1,7 +1,7 @@
 # Дано вещественное число R и массив вещественных чисел. Найти элемент массива,
 # который наиболее близок к данному числу.
 array = [1, 3, 3.5, 7, 32.4, 1.7, -3, -3.4, 1, 2, 0, 321]
-number_r = 50
+number_r = 1.5
 if number_r <= array.max
   array.push(number_r).sort!.inspect
   index_r = array.index(number_r)
@@ -11,13 +11,13 @@ if number_r <= array.max
 
   positive_before_number = before_number - before_number - before_number
 
-  if number_r == array.first
-    closest_number = array[index_r + 1]
-  elsif positive_before_number < after_number
-    closest_number = array[index_r - 1]
-  else
-    closest_number = array[index_r + 1]
-  end
+  closest_number = if number_r == array.first
+                     array[index_r + 1]
+                   elsif positive_before_number < after_number
+                     array[index_r - 1]
+                   else
+                     array[index_r + 1]
+                   end
 else
   closest_number = array.max
 end
@@ -25,4 +25,4 @@ end
 puts "Number R: #{number_r}"
 array.delete(number_r)
 puts array.inspect
-puts "The closest number to the number R #{closest_number}"
+puts "The closest number to the number R: #{closest_number}"
