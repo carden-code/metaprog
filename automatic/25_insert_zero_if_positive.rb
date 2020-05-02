13d5fe40f7361687c2acc47c066ecacfadfbaf4a
carden <carden.ruby@gmail.com>
2020-05-02 23:55:05 +0300

    not complited

--- automatic/25_insert_zero_if_positive.rb
+++ automatic/25_insert_zero_if_positive.rb
@@ -1,7 +1,5 @@
 # Дан целочисленный массив. Преобразовать его, вставив перед каждым
 # положительным элементом нулевой элемент.
-array = [1, 2, -15, 23, 0, -2, 4, 88, -2, 12, 11]
-arr = []
-array.each { |elem| arr << elem if elem.positive? }
-arr.each { |elem| array.insert(array.index(elem), array.first) }
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+array.each_with_index { |elem, index| array.push(array.first) if elem.positive? }
 puts array.inspect
