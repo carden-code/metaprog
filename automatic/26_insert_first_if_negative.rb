32105ad4b9b747c1a12acd8707eb6323cc96cb9a
carden <carden.ruby@gmail.com>
2020-05-06 23:01:14 +0300

    commit

--- automatic/26_insert_first_if_negative.rb
+++ automatic/26_insert_first_if_negative.rb
@@ -1,5 +1,5 @@
 # Дан целочисленный массив. Преобразовать его, вставив перед каждым
 # отрицательным элементом нулевой элемент.
 array = [1, 2, -15, 23, 0, -2, 4, 88, -2, 12, 11]
-array.each { |elem| array.insert(array.index(elem), array.first) if elem.negative? }
+array.each { |elem| array.insert(array.find_index(&:positive?), array.first)
 puts array.inspect
