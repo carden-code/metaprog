e39289ca539b8ed8d8127471a97dce1e62ab4ddf
carden <carden.ruby@gmail.com>
2020-05-07 23:12:33 +0300

    first commit

---
+++ automatic/47_min_elem_interval.rb
@@ -0,0 +1,7 @@
+# Дан целочисленный массив и интервал a..b.
+# Найти минимальный из элементов в этом интервале.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+interval = -1..10
+elem_interval = []
+array.each { |elem| elem_interval << elem if interval.include? elem }
+puts elem_interval.min
