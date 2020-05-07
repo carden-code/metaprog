10228f35bd8f440fd312875ffe98a0c1fa00a52d
carden <carden.ruby@gmail.com>
2020-05-07 23:28:55 +0300

    first commit

---
+++ automatic/50_number_elem_first_max.rb
@@ -0,0 +1,10 @@
+# Дан целочисленный массив. Найти количество элементов,
+# расположенных перед первым максимальным.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+max_elements = array.max(array.size)
+
+array.each do |elem|
+  break if elem == max_elements.first
+
+  puts elem
+end
