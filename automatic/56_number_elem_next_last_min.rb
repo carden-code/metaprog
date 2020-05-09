7d481032049db3422750def94ca08d3fd2bd9b36
carden <carden.ruby@gmail.com>
2020-05-09 23:22:22 +0300

    first commit

---
+++ automatic/56_number_elem_next_last_min.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Найти количество элементов,
+# расположенных после последнего минимального.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+min_elements = array.min(array.size)
+arr = array.index(min_elements.last)...array.index(array.last)
+puts arr.size
