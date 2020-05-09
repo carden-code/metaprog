f5f379e44eecbe132881079bbb013490e8c724e3
carden <carden.ruby@gmail.com>
2020-05-09 23:19:58 +0300

    first commit

---
+++ automatic/55_number_elem_next_last_max.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Найти количество элементов,
+# расположенных после последнего максимального.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+max_elements = array.max(array.size)
+arr = array.index(max_elements.last)...array.index(array.last)
+puts arr.size
