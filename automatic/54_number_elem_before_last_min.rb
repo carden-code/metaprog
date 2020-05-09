3f7b5641416cb38067ecb6624ac6c8fa26b7a248
carden <carden.ruby@gmail.com>
2020-05-09 23:15:45 +0300

    first commit

---
+++ automatic/54_number_elem_before_last_min.rb
@@ -0,0 +1,10 @@
+# Дан целочисленный массив. Найти количество элементов,
+# расположенных перед последним минимальным.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+min_elements = array.min(array.size)
+
+array.each do |elem|
+  break if elem == min_elements.last
+
+  puts elem
+end
