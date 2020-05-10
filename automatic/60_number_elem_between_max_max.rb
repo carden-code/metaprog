f5663262f2f4d04649fded50d33e4baa7674dc63
carden <carden.ruby@gmail.com>
2020-05-10 23:58:42 +0300

    not complited

---
+++ automatic/60_number_elem_between_max_max.rb
@@ -0,0 +1,10 @@
+# Дан целочисленный массив. Найти количество элементов, между первым и последним
+# максимальным.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+
+max_elements = array.max(array.size)
+interval = array.index(max_elements.first)..array.index(max_elements.last)
+puts array[interval.first]
+puts array[interval.last]
+b = array.max...array.last
+b.each {|elem| puts elem}
