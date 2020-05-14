f80b32fe3ef153964b5d0070038e5c5ad413bf94
carden <carden.ruby@gmail.com>
2020-05-14 23:15:32 +0300

    alternate

---
+++ automatic/67_even_alternate_odd_eve.rb
@@ -0,0 +1,12 @@
+# Дан целочисленный массив.
+# Проверить, чередуются ли в нем чётные и нечётные числа.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+alternete = 0
+array.each_index do |index|
+  if array[index - 1].odd? && array[index].even? ||
+     array[index - 1].even? && array[index].odd?
+    alternete += 1
+  end
+end
+
+puts alternete.zero? ? false : true
