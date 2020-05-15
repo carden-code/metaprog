5f387dc9e01b0b4a7f75b89ede1dce122ae0b89a
carden <carden.ruby@gmail.com>
2020-05-15 23:27:05 +0300

    not complited

---
+++ automatic/63_max_consecutive_elem.rb
@@ -0,0 +1,7 @@
+# Дан целочисленный массив.
+# Найти максимальное количество подряд идущих минимальных элементов.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+min_elements = array.min(array.size)
+array.each do |elem|
+end
+puts min_elements
