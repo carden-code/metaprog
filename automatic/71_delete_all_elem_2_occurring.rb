f7b7fec8343c9477cf4a30596996b81a0f4de1dd
carden <carden.ruby@gmail.com>
2020-05-17 23:55:18 +0300

    first commit

---
+++ automatic/71_delete_all_elem_2_occurring.rb
@@ -0,0 +1,6 @@
+# Дан целочисленный массив. Удалить все элементы, встречающиеся менее двух раз.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, -15, 12, 11]
+arr = []
+array.sort!.each_with_index { |elem, index| arr << elem if elem == array[index + 1] }
+array.sort.each { |elem| array.delete(elem) unless arr.any? elem }
+puts array.inspect
