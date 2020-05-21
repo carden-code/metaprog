6794fd1146cb69f2670e5da7bffcf2fba77815cd
carden <carden.ruby@gmail.com>
2020-05-21 23:22:15 +0300

    first commit

---
+++ automatic/80_interval_to_a_ascending.rb
@@ -0,0 +1,8 @@
+# Дан диапазон a..b. Получить массив из чисел, расположенных в этом диапазоне
+# (исключая сами эти числа), в порядке их возрастания, а также размер этого массива.
+puts range = -5...8
+product = range.to_a
+product.shift
+product.sort!
+puts product.inspect
+puts "array size: #{product.size}"
