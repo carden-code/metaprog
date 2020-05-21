b5e4f4be2d0df86516fa7ffdb1107dad6d44b7fe
carden <carden.ruby@gmail.com>
2020-05-21 23:22:36 +0300

    delete

--- automatic/80_interval_to_a_descending.rb
+++
@@ -1,8 +0,0 @@
-# Дан диапазон a..b. Получить массив из чисел, расположенных в этом диапазоне (
-# исключая сами эти числа), в порядке их убывания, а также размер этого массива.
-puts range = -5...8
-product = range.to_a
-product.shift
-product.sort! { |a, b| b <=> a }
-puts product.inspect
-puts "array size: #{product.size}"
