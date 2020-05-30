be04d705d1796fd428edb272e5051b8df047bb94
carden <carden.ruby@gmail.com>
2020-05-30 22:54:22 +0300

    arithmetic

---
+++ automatic/86_arithmetic_mean_of_numbers.rb
@@ -0,0 +1,4 @@
+# Дан целочисленный массив. Найти среднее арифметическое его элементов.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+arithmetic_mean = array.sum / array.size
+puts arithmetic_mean
