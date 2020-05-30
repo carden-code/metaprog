c885b287c18fb4235460ddd00d42b107e0bdfa9d
carden <carden.ruby@gmail.com>
2020-05-30 23:00:34 +0300

    first commit

---
+++ automatic/87_all_even_elements.rb
@@ -0,0 +1,5 @@
+# Дан целочисленный массив. Найти все чётные элементы.
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+even_elements = []
+array.each { |elem| even_elements << elem if elem.even? }
+puts even_elements
