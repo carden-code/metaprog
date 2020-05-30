6c2a293806c3947f8f9bf956956be92cda6bf9fc
carden <carden.ruby@gmail.com>
2020-05-30 23:20:01 +0300

    first commit

---
+++ automatic/91_number_k_true?.rb
@@ -0,0 +1,8 @@
+# Дан целочисленный массив и число К. Если существует элемент, меньший К,
+# то вывести true; в противном случае вывести false.
+puts 'Введите число К:'
+input = gets.chomp.to_i
+counter = 0
+array = [1, 2, -15, 23, 4, 0, -2, 4, 88, -2, 12, 11]
+array.each { |elem| counter += 1 if elem < input }
+puts counter.zero? ? 'false' : 'true'
